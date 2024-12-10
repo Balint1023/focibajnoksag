b4643335f392fc4866394d2913ceae40397721c1
HosszuDominik <dominikhosszu@gmail.com>
2024-12-10 16:17:10 +0100

    még van pár teendő de a html css rész már kész
    Nagyából mindent bekommenteltem

--- code.js
+++ code.js
@@ -1,16 +1,31 @@
 let count1 = 0; // golok
 
+
         // plusz
         function increase1() {
             count1++;
             document.getElementById('eredmenyelso').innerText = count1;
         }
 
-        //minus
+        //minusz
         function decrease1() {
             
             if(count1>0){
                 count1--;
             }
             document.getElementById('eredmenyelso').innerText = count1;
+        }
+
+        function Lap(){
+            document.getElementById("lapkeres").style.display = "block";
+        }
+        function Megse(){
+            document.getElementById("lapkeres").style.display = "none"
+            document.getElementById("lap").reset()
+        }
+        
+        function Start(){
+            document.getElementById("main2").style.display = "block"
+            document.getElementById("main1").style.display = "none"            
+            
         }
